ee9da87dd931d83e9f50b1fa25e9db938da1ead8
Ícaro Freire <icarovf@usp.br>
2020-07-24 19:23:37 -0300

    prettier code

--- tradingbot.py
+++ tradingbot.py
@@ -16,23 +16,19 @@ class StocksData():
         return
 
     def download_data(self, ticker, res, limit=None, start=None, end=None, after=None, until=None):
-        try:
-            barset = self.api.get_barset(ticker, timeframe=res, limit=limit, start=start, end=end, after=after, until=until)
-            stocks = { 'o': [], 'c': [], 'h': [], 'l': [], 'v': [], 't': [] }
-            for bar in barset[ticker]:
-                stocks['o'].append(bar.o)
-                stocks['c'].append(bar.c)
-                stocks['h'].append(bar.h)
-                stocks['l'].append(bar.l)
-                stocks['v'].append(bar.v)
-                stocks['t'].append(bar.t)
-            return stocks
-        except Exception as e:
-            print('Exception: {}'.format(e))
-        return
+        barset = self.api.get_barset(ticker, timeframe=res, limit=limit, start=start, end=end, after=after, until=until)
+        stocks = { 'o': [], 'c': [], 'h': [], 'l': [], 'v': [], 't': [] }
+        for bar in barset[ticker]:
+            stocks['o'].append(bar.o)
+            stocks['c'].append(bar.c)
+            stocks['h'].append(bar.h)
+            stocks['l'].append(bar.l)
+            stocks['v'].append(bar.v)
+            stocks['t'].append(bar.t)
+        return stocks
 
     def load_data(self, ticker, res, limit=None, start=None, end=None, after=None, until=None):
-        self.hist_data[ticker] = self.download_data(ticker, res, limit=None, start=None, end=None, after=None, until=None)
+        self.hist_data[ticker] = self.download_data(ticker, res, limit=limit, start=start, end=end, after=after, until=until)
         return
 
     def update_data(self, ticker, res, after):
@@ -45,24 +41,122 @@ class StocksData():
         self.hist_data[ticker]['t'] += stocks['t']
         return
 
-'''
-    ORDER(
-        type,
-        limit_price,
-        execution
-    )
-'''
-# Controls bot's trading logic
-class TradingController(Thread):
+# Manages local trading emulation
+class LocalTrader():
     def __init__(self, api, tradables):
-        super(TradingController, self).__init__()
+        self.api = api
+        self.tradables = tradables
+        self.stocks = StocksData(api)
+        self.orders = []
+        self.create_wallet(1000.0)
+        return
+
+     # Initializes wallet
+    def create_wallet(self, buying_power):
+        self.buying_power = buying_power
+        self.equity = buying_power
+        self.portfolio = {}
+        return
+
+    # Loads market data from Alpaca
+    def load_data(self, limit=None, start=None, end=None, after=None, until=None):
+        for ticker in self.tradables:
+            self.stocks.load_data(ticker, '1Min', limit=limit)
+        return
+
+
+    def get_equity(self):
+        equity = 0.0
+        for stock in self.portfolio:
+            equity += self.portfolio[stock]['quantity'] * self.portfolio[stock]['current_price']
+        return equity
+
+    def process_order(self, type, ticker, price, quantity):
+        if type == 'buy':
+            if self.buying_power >= price * quantity:
+                self.buying_power -= price * quantity
+                if ticker in self.portfolio:
+                    self.portfolio[ticker]['avg_entry_price'] = (self.portfolio[ticker]['avg_entry_price'] * self.portfolio[ticker]['quantity'] + price * quantity) / (self.portfolio[ticker]['quantity'] + quantity)
+                    self.portfolio[ticker]['quantity'] += quantity
+                    self.portfolio[ticker]['current_price'] = price
+                else:
+                    self.portfolio[ticker] = {
+                        'avg_entry_price': price,
+                        'quantity': quantity,
+                        'current_price': price
+                    }
+        elif type == 'sell':
+            if ticker in self.portfolio:
+                if self.portfolio[ticker]['quantity'] >= quantity:
+                    self.buying_power += price * quantity
+                    self.portfolio[ticker]['quantity'] -= quantity
+                    self.portfolio[ticker]['current_price'] = price
+        return
+
+    def bracket_order(self, ticker, price, quantity, take_profit, stop_loss):
+        exec_id = len(self.orders)
+        self.orders.append(['buy', ticker, price, quantity, 'instant', False, None, exec_id])
+        self.orders.append(['sell', ticker, take_profit['limit_price'], quantity, 'conditional', False, 'profit', exec_id + 1])
+        self.orders.append(['sell', ticker, stop_loss['limit_price'], quantity, 'conditional', False, 'loss', exec_id + 1])
+        return
+
+    def execute_order(self, order):
+        self.process_order(order[0], order[1], order[2], order[3])
+        for _order in self.orders:
+            if _order[7] == order[7]: self.orders.remove(_order)
+        order[5] = True
+        return
+
+    def execute_orders(self, t):
+        for order in self.orders:
+            if order[5] == False:
+                price = self.stocks.hist_data[order[1]]['c'][t]
+                if order[4] == 'instant': self.execute_order(order)
+                elif order[4] == 'conditional':
+                    if order[6] == 'profit':
+                        if price >= order[2]: self.execute_order(order)
+                    if order[6] == 'loss':
+                        if price <= order[2]: self.execute_order(order)
+        return
+
+
+    def trade(self):
+        for t in range(len(self.stocks.hist_data[self.tradables[0]]['t'])):
+            for ticker in self.tradables:
+                price = self.stocks.hist_data[ticker]['c'][t]
+                if self.buying_power > 0:
+                    self.bracket_order(
+                        ticker,
+                        price,
+                        self.buying_power / (len(self.tradables) * price),
+                        take_profit={
+                            'limit_price': price * 1.001
+                        },
+                        stop_loss={
+                            'stop_price': price * 0.99,
+                            'limit_price': price * 0.98
+                        }
+                    )
+            self.execute_orders(t)
+        return
+
+    def result(self):
+        return self.get_equity()
+
+# Emulates using real-time market data
+class PaperTrader():
+    def __init__(self, api, tradables):
         self.api = api
         self.tradables = tradables
         self.portfolio = {}
-        self.orders = []
         self.stocks = StocksData(api)
-        self.local_trading = False
         self.sync()
+        return
+
+    def sync(self):
+        self.sync_account()
+        self.sync_clock()
+        self.sync_equity()
         return
 
     def sync_account(self):
@@ -93,69 +187,7 @@ class TradingController(Thread):
                 print('Exception: {}'.format(e))
         return
 
-    def sync(self):
-        self.sync_account()
-        self.sync_clock()
-        self.sync_equity()
-        return
-
-    def local_order(self, type, ticker, price, quantity):
-        if type == 'buy':
-            if self.buying_power >= price * quantity:
-                self.buying_power -= price * quantity
-                if ticker in self.portfolio:
-                    self.portfolio[ticker]['avg_entry_price'] = (self.portfolio[ticker]['avg_entry_price'] * self.portfolio[ticker]['quantity'] + price * quantity) / (self.portfolio[ticker]['quantity'] + quantity)
-                    self.portfolio[ticker]['quantity'] += quantity
-                    self.portfolio[ticker]['current_price'] = price
-                else:
-                    self.portfolio[ticker] = {
-                        'avg_entry_price': price,
-                        'quantity': quantity,
-                        'current_price': price
-                    }
-        elif type == 'sell':
-            if ticker in self.portfolio:
-                if self.portfolio[ticker]['quantity'] >= quantity:
-                    self.buying_power += price * quantity
-                    self.portfolio[ticker]['quantity'] -= quantity
-                    self.portfolio[ticker]['current_price'] = price
-        return
-
-    '''
-        take_profit
-            limit_price
-        stop_loss
-            stop_price
-            limit_price
-    '''
-    def local_bracket(self, ticker, price, quantity, specs):
-        exec_id = len(self.orders)
-        self.orders.append(['buy', ticker, price, quantity, 'instant', False, None, exec_id])
-        self.orders.append(['sell', ticker, specs['take_profit']['limit_price'], quantity, 'conditional', False, 'profit', exec_id + 1])
-        self.orders.append(['sell', ticker, specs['stop_loss']['limit_price'], quantity, 'conditional', False, 'loss', exec_id + 1])
-        return
-
-    def execute_orders(self, t):
-        for order in self.orders:
-            if order[5] == False:
-                if order[4] == 'instant':
-                    self.local_order(order[0], order[1], order[2], order[3])
-                    order[5] = True
-                elif order[4] == 'conditional':
-                    if order[6] == 'loss':
-                        if self.stocks.hist_data[order[1]]['c'][t] <= order[2]:
-                            self.local_order(order[0], order[1], order[2], order[3])
-                            order[5] = True
-                    if order[6] == 'profit':
-                        if self.stocks.hist_data[order[1]]['c'][t] >= order[2]:
-                            self.local_order(order[0], order[1], order[2], order[3])
-                            order[5] = True
-                    '''for i in range(len(self.orders)):
-                        if order[7] == self.orders[i][7]: print(order[7])'''
-
-        return
-
-    def paper_trade(self):
+    def trade(self):
         for ticker in self.tradables: self.stocks.load_data(ticker, '1Min', after=self.prev_open)
 
         while self.is_open:
@@ -173,7 +205,7 @@ class TradingController(Thread):
                                 type='market',
                                 time_in_force='gtc',
                                 take_profit={
-                                    'limit_price': price * 1.005
+                                    'limit_price': price * 1.01
                                 },
                                 stop_loss={
                                     'stop_price': price * 0.99,
@@ -184,45 +216,34 @@ class TradingController(Thread):
                         except Exception as e:
                             print('Exception: {}'.format(e))
             self.sync()
+            print('Equity: {}'.format(self.equity))
             time.sleep(60)
-
         return
 
-    def local_trade(self):
-        self.local_trading = True
-
-        limit = 350
-        for ticker in self.tradables: self.stocks.load_data(ticker, '1Min', limit=limit)
-
-        for t in range(len(self.stocks.hist_data[self.tradables[0]]['t'])):
-            for ticker in self.tradables:
-                price = self.stocks.hist_data[ticker]['c'][t]
-                if self.buying_power > 0:
-                    self.local_bracket(
-                        ticker,
-                        price,
-                        self.buying_power / (len(self.tradables) * price),
-                        {
-                            'take_profit': {
-                                'limit_price': price * 1.01
-                            },
-                            'stop_loss': {
-                                'stop_price': price * 0.99,
-                                'limit_price': price * 0.98
-                            }
-                        }
-                    )
-            self.execute_orders(t)
-            print(self.orders)
+# Controls bot's trading logic
+class TradingController():
+    def __init__(self, api, tradables):
+        self.api = api
+        self.tradables = tradables
+        self.sync()
         return
 
-    def run(self):
-        while self.is_alive:
-            try:
-                if self.is_open:
-                    self.paper_trade()
-            except Exception as e:
-                print('Exception: {}'.format(e))
+    def sync(self):
+        self.is_open = self.api.get_clock().is_open
+        return
+
+    def trade(self):
+        try:
+            if self.is_open:
+                paper_trade = PaperTrader(self.api, self.tradables)
+                paper_trade.trade()
+            else:
+                local_trade = LocalTrader(self.api, self.tradables)
+                local_trade.load_data(limit=350)
+                local_trade.trade()
+                print(local_trade.result())
+        except Exception as e:
+            print('Exception: {}'.format(e))
         return
 
 # Main controller
@@ -232,7 +253,7 @@ class Controller():
             self.api = tradeapi.REST(self.config['auth']['api_key'], self.config['auth']['secret_key'], self.config['auth']['endpoint'])
             print('Logged to Alpaca successfully!')
             self.tradectl = TradingController(self.api, self.config['tradables'])
-            self.tradectl.start()
+            self.tradectl.trade()
         else: print('Bot is not setup properly.')
         return
 
@@ -245,8 +266,5 @@ class Controller():
                 print('Exception: {}'.format(e))
                 return False
 
-    def close(self):
-        self.tradectl.join()
 
 controller = Controller()
-controller.close()
